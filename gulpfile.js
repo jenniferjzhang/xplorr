ee3b1ab7c657f5c0d26d91801329f5cdd0217c64
Tilly Taylor <tillyt@mit.edu>
2016-04-28 14:12:56 -0400

    fix issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
 var browserSync = require('browser-sync');
 var del = require('del');
-var _wiredep = require('wiredep');
+var wiredep = require('wiredep');
 var $ = require('gulp-load-plugins')();
 var reload = browserSync.reload;
 
 gulp.task('html', function () {
     return gulp.src('app/*.html').pipe($.useref({ searchPath: ['.tmp', 'app', '.'] })).pipe($.if('*.js', $.uglify())).pipe($.if('*.css', $.cssnano())).pipe($.if('*.html', $.htmlmin({ collapseWhitespace: true }))).pipe(gulp.dest('dist'));
-});
-
-gulp.task('images', function () {
-    return gulp.src('app/images/**/*').pipe($.cache($.imagemin({
-        progressive: true,
-        interlaced: true,
-        // don't remove IDs from SVGs, they are often used
-        // as hooks for embedding and styling
-        svgoPlugins: [{ cleanupIDs: false }]
-    }))).pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('extras', function () {
@@ -30,7 +19,7 @@ gulp.task('extras', function () {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', function () {
+gulp.task('serve', ['build'], function () {
     (0, browserSync)({
         notify: false,
         port: 9000,
@@ -42,7 +31,7 @@ gulp.task('serve', function () {
         }
     });
 
-    gulp.watch(['app/*.html', 'app/images/**/*', '.tmp/fonts/**/*']).on('change', reload);
+    gulp.watch(['app/*.html', 'app/images/**/*', 'app/styles/*', 'app/scripts/*', '.tmp/fonts/**/*']).on('change', reload);
 
     gulp.watch('bower.json', ['wiredep']);
 });
@@ -57,35 +46,21 @@ gulp.task('serve:dist', function () {
     });
 });
 
-gulp.task('serve:test', function () {
-    (0, browserSync)({
-        notify: false,
-        port: 9000,
-        ui: false,
-        server: {
-            baseDir: 'test',
-            routes: {
-                '/scripts': '.tmp/scripts',
-                '/bower_components': 'bower_components'
-            }
-        }
-    });
-
-    gulp.watch('test/spec/**/*.js').on('change', reload);
-});
-
 // inject bower components
 gulp.task('wiredep', function () {
-    gulp.src('app/styles/*.scss').pipe((0, _wiredep.stream)({
+    gulp.src('app/styles/*.scss').pipe((0, wiredep.stream)({
         ignorePath: /^(\.\.\/)+/
     })).pipe(gulp.dest('app/styles'));
 
-    gulp.src('app/*.html').pipe((0, _wiredep.stream)({
+    gulp.src('app/*.html').pipe((0, wiredep.stream)({
         ignorePath: /^(\.\.\/)*\.\./
     })).pipe(gulp.dest('app'));
+
+    gulp.src('app/index.html')
 });
 
-gulp.task('build', ['html', 'images', 'extras'], function () {
+
+gulp.task('build', ['html', 'extras', 'wiredep'], function () {
     return gulp.src('dist/**/*');
 });
 
