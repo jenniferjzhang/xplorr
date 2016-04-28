92e4156c6652df8260620660278463ba910b507e
Tilly Taylor <tillyt@mit.edu>
2016-04-28 12:55:16 -0400

    gulpfile revert

--- gulpfile.js
+++ gulpfile.js
@@ -8,151 +8,87 @@ var _wiredep = require('wiredep');
 var $ = require('gulp-load-plugins')();
 var reload = browserSync.reload;
 
-gulp.task('styles', function () {
-  return gulp.src('app/styles/*.scss')
-    .pipe($.plumber())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('scripts', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
-});
-
-function lint(files, options) {
-  return function () {
-    return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
-      .pipe($.eslint(options))
-      .pipe($.eslint.format())
-      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-  };
-}
-const testLintOptions = {
-  env: {
-    mocha: true
-  }
-};
-
-gulp.task('lint', lint('app/scripts/**/*.js'));
-gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
-
-gulp.task('html', ['styles', 'scripts'], function () {
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
+gulp.task('html', function () {
+    return gulp.src('app/*.html').pipe($.useref({ searchPath: ['.tmp', 'app', '.'] })).pipe($.if('*.js', $.uglify())).pipe($.if('*.css', $.cssnano())).pipe($.if('*.html', $.htmlmin({ collapseWhitespace: true }))).pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function () {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', function () {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'));
+    return gulp.src('app/images/**/*').pipe($.cache($.imagemin({
+        progressive: true,
+        interlaced: true,
+        // don't remove IDs from SVGs, they are often used
+        // as hooks for embedding and styling
+        svgoPlugins: [{ cleanupIDs: false }]
+    }))).pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('extras', function () {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
+    return gulp.src(['app/*.*', '!app/*.html'], {
+        dot: true
+    }).pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', ['styles', 'scripts', 'fonts'], function () {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
+gulp.task('serve', function () {
+    (0, browserSync)({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['.tmp', 'app'],
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
 
-  gulp.watch([
-    'app/*.html',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
+    gulp.watch(['app/*.html', 'app/images/**/*', '.tmp/fonts/**/*']).on('change', reload);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
 gulp.task('serve:dist', function () {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
+    (0, browserSync)({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['dist']
+        }
+    });
 });
 
-gulp.task('serve:test', ['scripts'], function () {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
+gulp.task('serve:test', function () {
+    (0, browserSync)({
+        notify: false,
+        port: 9000,
+        ui: false,
+        server: {
+            baseDir: 'test',
+            routes: {
+                '/scripts': '.tmp/scripts',
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
 
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
+    gulp.watch('test/spec/**/*.js').on('change', reload);
 });
 
 // inject bower components
 gulp.task('wiredep', function () {
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
+    gulp.src('app/styles/*.scss').pipe((0, _wiredep.stream)({
+        ignorePath: /^(\.\.\/)+/
+    })).pipe(gulp.dest('app/styles'));
 
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
+    gulp.src('app/*.html').pipe((0, _wiredep.stream)({
+        ignorePath: /^(\.\.\/)*\.\./
+    })).pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], function () {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['html', 'images', 'extras'], function () {
+    return gulp.src('dist/**/*');
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
 });
