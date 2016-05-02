4f9df18da1f5bb5a52c9951a4b5da58c93c81590
treeshuh <tricia.shi@gmail.com>
2016-05-02 04:23:00 -0400

    basic functionality for the quiz

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,11 +7,65 @@ var ctx; var radarData; var myRadarChart;
 var datasetIndex = 0;
 var traits = ["tempo", "acousticness", "danceability", "energy", "happiness"];
 
+var quizProgress = -1;
+var quizQuestions = [];
+var quizResults = {limit: 5};
+var QUESTION_CONTAINER = 'question-container';
+
 // The quiz doesn't do anything right now
 // TODO[tricia]: actually implement the quiz.
 var startQuiz = function() {
   $('#login-container').hide();
-  $('body').append('<button id="finish-quiz">Quiz goes here. Skip for now</button>');
+  nextQuizQuestion();
+
+};
+
+// questionText is a string
+// questionList is an array of dicts {text: , val: }
+// questionClass is a string to use as a jQuery identifier for the questions
+var makeQuizHtml = function(questionText, questionList, questionClass, id, extra) {
+  id = id || '';
+  extra = extra || '';
+  var text = '<div class="'+QUESTION_CONTAINER+'" id="'+id+'">';
+  text +='<h2 class="question-text">'+questionText+'</h2>';
+  for (var i=0; i<questionList.length; i++) {
+    text += '<div class="'+questionClass+' quiz-answer" data-value='+questionList[i].val+'>'+questionList[i].text+'</div>'
+  }
+  text += extra;
+  text += '</div>'; //end question-container
+  return text;
+}
+
+var makeQuizInput = function(questionText, inputLabel, numLabels, questionClass, id, extra) {
+  id = id || '';
+  extra = extra || '';  
+  var text = '<div class="'+QUESTION_CONTAINER+'" id="'+id+'">';
+  text +='<h2 class="question-text">'+questionText+'</h2>';
+  for (var i=0; i<numLabels; i++) {
+    text += '<input type="text" class="'+questionClass+' quiz-answer" placeholder="'+inputLabel+'"></input>';
+  }
+  text += '<button class="btn '+questionClass+'">Next</button>';
+  text += extra;
+  text += '</div>'; //end question-container
+  return text;
+}
+
+var nextQuizQuestion = function() {
+  quizProgress += 1;
+  if (quizProgress > 0) {
+    $('.'+QUESTION_CONTAINER).hide();
+  }
+
+  if (quizProgress < quizQuestions.length) {
+    quizQuestions[quizProgress]();
+  } 
+  else {
+    finishQuiz(); 
+  }
+}
+
+var finishQuiz = function() {
+/*  $('body').append('<button id="finish-quiz">Quiz goes here. Skip for now</button>');
 
   $('#finish-quiz').on('click', function(e){
       e.preventDefault();
@@ -23,8 +77,137 @@ var startQuiz = function() {
             {id: 'spotify:track:717TY4sfgKQm4kFbYQIzgo'}
             ];
       setUpExploration();
+  });  */
+  console.log(quizResults);
+  spotifyApi.getRecommendations(quizResults, processQuiz);
+}
+
+var processQuiz = function(err, data) {
+  songList = data.tracks;
+  $.each(songList, function(){
+    this.id = 'spotify:track:'+this.id;
   });
-};
+  setUpExploration();
+}
+
+var quizValence = function() {
+  var questionText = "How are you feeling?";
+  var questionList = [
+    {text: 'morose', val: 0},
+    {text: 'sad', val: 0.25},
+    {text: 'content', val: 0.50},
+    {text: 'happy', val: 0.75},
+    {text: 'ecstatic', val: 1.0}
+  ];
+  var questionClass = 'valence-quiz-answer';
+  $('body').append(makeQuizHtml(questionText, questionList, questionClass));
+  $('.'+questionClass).on('click', function(e) {
+    e.preventDefault();
+    quizResults.target_valence = $(e.target).attr('data-value');
+    nextQuizQuestion();
+  });
+}
+
+var quizDanceability = function() {
+  var questionText = "Do you want to dance?";
+  var questionList = [
+    {text: 'NO.', val: 0},
+    {text: 'nah.', val: 0.25},
+    {text: 'maybe...', val: 0.50},
+    {text: 'yes!', val: 0.75},
+    {text: 'all night long!', val: 1.0}
+  ];
+  var questionClass = 'danceability-quiz-answer';
+  $('body').append(makeQuizHtml(questionText, questionList, questionClass));
+  $('.'+questionClass).on('click', function(e) {
+    e.preventDefault();
+    quizResults.target_danceability = $(e.target).attr('data-value');
+    nextQuizQuestion();
+  });
+}
+
+var quizAcousticness = function() {
+  var questionText = "Do you want to sing along?";
+  var questionList = [
+    {text: 'no words is best.', val: 1.0},
+    {text: 'I don\'t sing', val: 0.7},
+    {text: 'if I know the words', val: 0.50},
+    {text: 'For sure!', val: 0.1},
+    {text: 'I\'m ready to sing my heart out!', val: 0.0}
+  ];
+  var questionClass = 'acousticness-quiz-answer';
+  $('body').append(makeQuizHtml(questionText, questionList, questionClass));
+  $('.'+questionClass).on('click', function(e) {
+    e.preventDefault();
+    quizResults.target_acousticness = $(e.target).attr('data-value');
+    nextQuizQuestion();
+  });
+}
+
+var quizSeeds = function() {
+  // genres
+  spotifyApi.getAvailableGenreSeeds(finishSeeds);
+}
+
+var finishSeeds = function(err, data) {
+  // genres
+  var questionText1 = "choose a genre to explore";
+  var questionList1 = data.genres.map(function(obj) {
+    return {text: obj, val: obj};
+  });
+  var questionClass1 = 'genre-quiz-answer';
+  $('body').append(makeQuizHtml(questionText1, questionList1, questionClass1, 
+      'genre-seed',
+      '<button class="btn" id="to-artist-seed">explore artists instead</button>'));
+  $('.'+questionClass1).on('click', function(e) {
+    e.preventDefault();
+    quizResults.seed_genres = [$(e.target).attr('data-value')];
+    nextQuizQuestion();
+  });
+
+  // artists
+  var questionText2 = "choose artists to explore";
+  var inputLabel2 = "spotify artist uri";
+  var numArtists = 5;
+  var questionClass2 = "artist-quiz-answer";
+  $('body').append(makeQuizInput(questionText2, inputLabel2, numArtists, questionClass2,
+      'artist-seed',
+      '<button class="btn" id="to-genre-seed">explore genres instead</button>'));
+  $('button.'+questionClass2).on('click', function(e) {
+    e.preventDefault();
+    quizResults.seed_artists = [];
+    $.each($('input.'+questionClass2), function(){
+      // TODO: better sanitization
+      if (this.value && this.value != '') {
+        quizResults.seed_artists.push(this.value.split(':')[2]);
+      }
+    });
+
+    // TODO: better error message
+    if (quizResults.seed_artists.length > 0) {
+      nextQuizQuestion();
+    } else {
+      window.alert("you need at least one artist!");
+    }
+  });
+
+  $('#to-artist-seed').on('click', function(e) {
+    $('#artist-seed').show();
+    $('#genre-seed').hide();
+  });
+
+  $('#to-genre-seed').on('click', function(e) {
+    $('#genre-seed').show();
+    $('#artist-seed').hide();
+  });
+
+  $('#artist-seed').hide();
+}
+
+quizQuestions.push(quizValence);
+quizQuestions.push(quizDanceability);
+quizQuestions.push(quizAcousticness);
+quizQuestions.push(quizSeeds);
 
 // After the quiz, we set up the scene
 var setUpExploration = function() {
