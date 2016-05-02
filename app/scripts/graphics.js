7fee3594e3b9c65474540d3f0f6c171f9c24d0c5
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2016-05-01 22:33:11 -0400

    Up and down arrow keys move forward and backwards in hallway

--- app/scripts/graphics.js
+++ app/scripts/graphics.js
@@ -5,54 +5,41 @@ var camera = new THREE.PerspectiveCamera( 75, WIDTH/HEIGHT, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize( WIDTH, HEIGHT );
 document.body.appendChild( renderer.domElement );
+var future_z = -100
+var BLOCK_LENGTH = 2
 
 var FORWARD = 38; // up arrow key code
 var BACKWARD = 40; // down arrow key code
 var LEFT = 37;
 var RIGHT = 39;
+
 camera.position.z = 5;
 
 function addCube(x, y, z, side) {
-    var geometry = new THREE.BoxGeometry( 2, 5, 2 );
+    var geometry = new THREE.BoxGeometry( 2, 5, BLOCK_LENGTH );
     var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff} );
     var cube = new THREE.Mesh( geometry, material );
-    // if (side === 'left') {
-    //     cube.rotation.x +=1;
-    //     cube.rotation.y +=1;
-    // } else if (side === 'right') {
-    //     cube.rotation.x +=1;
-    //     cube.rotation.y +=1;
-    // }
     scene.add( cube );
     cube.position.x = x;
     cube.position.y = y;
     cube.position.z = z;
+    console.log("adding cubes at ", x, y, z)
 
 };
 
 
 var initScene = function () {
     //left side of hallway
-    for (i = 0; i > -100; i -= 2) {
+    for (i = 0; i > future_z; i -= BLOCK_LENGTH) {
         addCube(5, 0, i, 'right');
     }
-    for (i = 0; i > -100; i-= 2) {
+    for (i = 0; i > future_z; i-= BLOCK_LENGTH) {
         addCube(-5, 0, i, 'left');
     }
 };
 
 
 initScene();
-// addCube(1, 2, 0,'left');
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-// cube.position.x = 1;
-// cube.position.y = 2;
-// cube.position.z = 3;
-// console.log(cube);
-
 render();
 
 $(document).keydown(function(e) {
@@ -76,18 +63,24 @@ $(document).keydown(function(e) {
 var moveForward = function() {
     // add more cubes in the future
     // move camera forward
-    addCube(5, 0, -102, 'right')
-    addCube(-5, 0, -102, 'left')
+    addCube(5, 0, future_z, 'right')
+    addCube(-5, 0, future_z, 'left')
 
-    var curr_camera_z = camera.position.z;
+    future_z -= BLOCK_LENGTH
 
-    camera.position.z = curr_camera_z - 2;
+    camera.translateZ(-BLOCK_LENGTH);
+    render();
+
+    console.log("camera position", camera.position);
+
 
 
 };
 
 var moveBackward = function () {
     // move camera backward
+    camera.translateZ(BLOCK_LENGTH);
+    render();
 };
 
 // cube.position.set(1,2,1);
